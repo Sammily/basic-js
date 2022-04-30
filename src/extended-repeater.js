e9a0737b4d798392c93178764aa4e91d619bee00
Sammily <sammily03@yandex.ru>
2022-04-30 19:43:30 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+ function repeater(str, options) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  /*repeatTimes sets the number of repetitions of the str;
+separator is a string separating repetitions of the str;
+addition is an additional string that will be added to each repetition of the str;
+additionRepeatTimes sets the number of repetitions of the addition;
+additionSeparator is a string separating repetitions of the addition.*/
+
+let str2=[];
+if(str===null){
+  str='null';
+}
+if(options.addition===null){
+options.addition='null';
+}
+if(options.separator== undefined){
+  options.separator='+';
+}
+if(options.additionSeparator== undefined){
+  options.additionSeparator='|';
+}
+if(options.additionRepeatTimes== undefined){
+  options.additionRepeatTimes=1;
+}
+if(options.repeatTimes== undefined){
+  options.repeatTimes=1;
+}
+for(let i=0;i<options.repeatTimes;i++){
+  str2.push(str);
+  for(let j=0;j<options.additionRepeatTimes;j++){
+    str2.push(options.addition);
+    if(j!=options.additionRepeatTimes-1){str2.push(options.additionSeparator);}
+    
+  }
+  if(i!=options.repeatTimes-1){
+      str2.push(options.separator);
+  
+}}
+return str2.join('');
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(arr) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(!(Array.isArray(arr))){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  else{
+    if(arr==[]){
+      return [];
+    }
+    else{
+      let arr2 = arr;
+      for(let i=0;i<arr2.length;i++){
+        if(arr2[i]=='--discard-next'){
+          arr2.splice(i+1,1);
+        }
+        if(arr2[i]=='--discard-prev'){
+          arr2.splice(i-1,1);
+        }
+        if(arr2[i]=='--double-next'){
+          arr.splice(i+1,0,arr2[i+1]);
+        }
+        if(arr2[i]=='--double-prev'){
+          arr2.splice(i-1,0,arr2[i-1]);
+        }
+      }
+    }
+  return arr2;
+  }
+  
 }
 
 module.exports = {
   transform
 };
+
+/*--discard-next excludes the next element of the array from the transformed array.
+--discard-prev excludes the previous element of the array from the transformed array.
+--double-next duplicates the next element of the array in the transformed array.
+--double-prev duplicates the previous element of the array in the transformed array.*/
